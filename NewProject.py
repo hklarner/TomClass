c2ab9dd8f1173c1a1e24cae71fa7580263315b6b
Hannes Klarner <hannes.klarner@FU-Berlin.de>
2012-09-20 12:08:41 +0200

    uhum

--- NewProject.py
+++ NewProject.py
@@ -12,31 +12,11 @@ reload(RegulatoryGraphs)
 reload(ENUMERATOR)
 reload(DATABASE)
 
-class csv_writer():
-    def __init__(self, ProjectName, Model):
-        self.fname = os.path.join('Projects',ProjectName, ProjectName+'.csv')
-        self.f = open(self.fname, 'wb')
-        self.writer = csv.DictWriter(self.f, fieldnames=[str(p) for p in sorted(Model.parameters)], dialect='excel-tab')
-        header = dict([(str(p),str(p)) for p in sorted(Model.parameters, key=lambda x: str(x))])
-        self.writer.writerow(header)
-        print 'Saving results in', self.fname
-
-    def insert_parametrization(self, ParamDict):
-        row = dict([(str(k),v) for k,v in ParamDict.items()])
-        self.writer.writerow(row)
-
-    def close(self):
-        print 'Closing',self.fname,
-        self.f.close()
-        print 'ok'
-        
-
-
 
 
 def run():
-    PROJECT_NAME = 'Shahrad'
-    MODEL = RegulatoryGraphs.Shahrad()
+    PROJECT_NAME = 'Boolean4'
+    MODEL = RegulatoryGraphs.Boolean4()
     CONSTRAINT = 'Multiplex(A=1 and B=1, C=1 [C]) and (NotObservable(C,A,1) or ActivatingOnly(C,A,1)) and Activating(C,B,1) and NotInhibiting(C,B,2)'+\
                  'and Some(A>0 [C] = 0)'
     RegulatoryGraphs.PrintFullInfo(MODEL)
